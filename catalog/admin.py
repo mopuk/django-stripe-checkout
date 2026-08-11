6c2052db1d5897f24cbd63ef01f0dab943de463c
MopuK <stanislavgusev.sv@gmail.com>
2026-08-11 23:45:33 +0300

    feat: add ability to change all models to admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 
-from .models import Item
+from .models import Discount, Item, Order, OrderItem, Tax
 
 admin.site.register(Item)
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(Discount)
+admin.site.register(Tax)
